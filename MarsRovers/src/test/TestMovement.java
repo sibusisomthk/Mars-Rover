3533e5f436f6a212dd5b1b1868ac6b863223410d
Sibusiso.Malatji <Sibusiso.Malatji@boxfusion.io>
2021-04-06 22:32:08 +0200

    removed unused test case

--- MarsRovers/src/test/TestMovement.java
+++ MarsRovers/src/test/TestMovement.java
@@ -19,10 +19,4 @@ public class TestMovement {
     public void setUp() throws Exception {
 		
     }
-	@Test 
-    public void testUpperRightCoordinatesValid() {
-		int[] expectedValues = {5,5};
-		
-		assertArrayEquals("Valid Coordinates",expectedValues, Helper.ValidateUpperRightCoordinates("5 5"));          
-    }
 }
