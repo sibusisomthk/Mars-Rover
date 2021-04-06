7e93fcf213a6fdba1467510ab4cc6b5e55cf07af
Sibusiso.Malatji <Sibusiso.Malatji@boxfusion.io>
2021-04-06 22:27:19 +0200

    switch statement bug fix

--- MarsRovers/src/model/World.java
+++ MarsRovers/src/model/World.java
@@ -91,34 +91,46 @@ public class World {
     		switch(position.getDirection()) {
     		case NORTH:
     			position.setDirection(Direction.EAST);
+    			break;
     		case EAST:
-    		position.setDirection(Direction.SOUTH);
+    		   position.setDirection(Direction.SOUTH);
+    		   break;
     		case SOUTH:
     			position.setDirection(Direction.WEST);
+    			break;
     		case WEST:
     			position.setDirection(Direction.NORTH);
+    			break;
     		}
     	}else if(c=='L' || c=='l') {//Rotate rover
     		switch(position.getDirection()) {
     		case NORTH:
     			position.setDirection(Direction.WEST);
+    			break;
     		case EAST:
-    		position.setDirection(Direction.NORTH);
+	    		position.setDirection(Direction.NORTH);
+	    		break;
     		case SOUTH:
     			position.setDirection(Direction.EAST);
+    			break;
     		case WEST:
     			position.setDirection(Direction.SOUTH);
+    			break;
     		}
     	}else {//Move Rover
     		switch(position.getDirection()) {
     		case NORTH:
     			position.setY(position.getY()+1);
+    			break;
     		case EAST:
     			position.setX(position.getX()+1);
+    			break;
     		case SOUTH:
     			position.setY(position.getY()-1);
+    			break;
     		case WEST:
     			position.setX(position.getX()-1);
+    			break;
     		}
     		if(!Helper.IsPositionAvailable(this, position)) position = null;//this will trigger end of rover movement.
     	}
